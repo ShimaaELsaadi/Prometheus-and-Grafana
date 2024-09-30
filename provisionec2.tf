46d62e539ff7d0a2e1835e7c06afe25ad32d393f
Shaimaa Mahmoud Elsaadi <120274014+ShimaaELsaadi@users.noreply.github.com>
2024-09-30 21:27:05 +0300

    new ec2 by ansible

--- provisionec2.tf
+++ provisionec2.tf
@@ -7,14 +7,14 @@ resource "aws_instance" "prometheus_grafana" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = "PrometheusGrafanaa"
+    Name = "PrometheusGrafana"
   }
 key_name = "key1"
 
-vpc_security_group_ids = [aws_security_group.prometheus_grafannn.id]
+vpc_security_group_ids = [aws_security_group.prometheus_grafana.id]
 }
-resource "aws_security_group" "prometheus_grafannn" {
-  name        = "prometheus_grafannn"
+resource "aws_security_group" "prometheus_grafana" {
+  name        = "prometheus_grafana"
 
   ingress {
     from_port   = 9090
@@ -36,4 +36,14 @@ resource "aws_security_group" "prometheus_grafannn" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+resource "null_resource" "update_ansible_hosts" {
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "[ec2]" > ./hosts
+      echo "${aws_instance.prometheus_grafana.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/${aws_instance.prometheus_grafana.key_name}.pem" >> ./hosts
+    EOT
+  }
+
+  depends_on = [aws_instance.prometheus_grafana]
 }
